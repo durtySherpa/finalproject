efeb9507ef9d7862c1d10cc43ad54f3b9f1a3a63
Tim F. Miller <timothymiller382@gmail.com>
2014-12-11 14:42:51 -0500

    Update formatting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "USMA CS450"
+    base_title = "Weekly Reports"
     if page_title.empty?
       base_title
     else
